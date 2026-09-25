3d5209488ab3b29eb07ea418613085d811d1ffc2
Mariano Blua <mariano.blua@gmail.com>
2026-09-24 22:27:26 -0300

    test(sidebar): add data-ac-testid to session menu Coding Agent item (#2523)

--- src/sidebar/components/SessionItem.tsx
+++ src/sidebar/components/SessionItem.tsx
@@ -542,6 +542,8 @@ const SessionItem: Component<{
             <button
               class="session-context-option"
               onClick={handleCodingAgentRestart}
+              data-ac-testid={`session.${props.session.id}.coding-agent`}
+              data-ac-role="menuitem"
             >
               Coding Agent
             </button>
